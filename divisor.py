51ce2a93282af248030d4d546eee0949a314261d
Adison25 <emerickadison@gmail.com>
2021-04-15 17:36:25 -0700

    finished divisor

--- divisor.py
+++ divisor.py
@@ -1,3 +1,7 @@
-print("Please enter a number to find all divisors: ")
-num = input()
-print(num)
+num = input("Please enter a number to find all divisors: ")
+i = 1
+while i <= int(num):
+    if (int(num) % i == 0):
+        print(str(i) + " "),
+    i = i + 1
+    #print(i)
